0f2e7a8040ec9f5e1c4c66e33289d547add9a56e
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-28 12:52:41 +0000

    Update MongoDB connection string to use environment variable for database URL

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/qltcp', {
+        await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/qltcp', {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
