c6e37e1ccbcdcdcd74bb105fffe0604d53e9daab
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-28 12:54:42 +0000

    Update mongoose connect options to remove deprecated settings

--- db.js
+++ db.js
@@ -4,9 +4,7 @@ const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/qltcp', {
             useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-            useFindAndModify: false
+            useUnifiedTopology: true
         });
         console.log('MongoDB connected');
     } catch (err) {
